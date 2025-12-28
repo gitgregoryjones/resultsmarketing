1d1cd531a055cc4acbdd5e81adbc7aee7b73cb2f
gitgregoryjones <gregory.jones@synovas.com>
2025-12-28 10:19:58 -0500

    Remove layout persistence from admin save

--- admin/cms.js
+++ admin/cms.js
@@ -1536,7 +1536,6 @@
   saveButton.addEventListener('click', (event) => {
     event.preventDefault();
     saveSelection();
-    persistLayout();
   });
   deleteButton.addEventListener('click', (event) => {
     event.preventDefault();
